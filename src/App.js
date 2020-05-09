25bef5cc9dce583566c1ebe3c6a75a4173f2c069
ymgyt <yamaguchi7073xtt@gmail.com>
2020-05-09 21:47:16 +0900

    lesson 21

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import React, {Fragment, Component } from 'react';
-import './App.css';
+import React, {Fragment, Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from './component/layout/Navbar';
 import Users from "./component/users/Users";
+import Search from "./component/users/Search";
+import Alert from "./component/layout/Alert";
+import About from "./component/pages/About";
+import axios from 'axios';
+import './App.css';
 
-function App() {
-  return (
-      <div className='App'>
-        <Navbar title='Hello'  icon='fab fa-github' />
-        <div className='container'>
-            <Users />
-        </div>
-      </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  }
+
+  // async componentDidMount() {
+  //   this.setState({loading: true});
+  //
+  //   const res = await axios
+  //       .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({users: res.data, loading: false});
+  // }
+
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({loading: true})
+    const res = await axios
+        .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading: false})
+  }
+
+  // Clear users from state
+  clearUsers = () => this.setState({users: [], loading: false})
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg: msg, type: type}});
+    setTimeout(() => this.setState({alert: null}), 5000);
+  };
+
+  render() {
+    const {users, loading} = this.state;
+    return (
+        <Router>
+          <div className='App'>
+            <Navbar title='Github finder' icon='fab fa-github'/>
+            <div className='container'>
+              <Alert alert={this.state.alert}/>
+              <Switch>
+                <Route exact path='/' render={props => (
+                    <Fragment>
+                      <Search
+                          searchUsers={this.searchUsers}
+                          clearUsers={this.clearUsers}
+                          showClear={this.state.users.length > 0 ? true : false}
+                          setAlert={this.setAlert}
+                      />
+                      <Users loading={loading} users={users}/>
+                    </Fragment>
+                )}/>
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
--- src/component/users/UserItem.js
+++ src/component/users/UserItem.js
@@ -1,26 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-        return (
-            <div>
-                <div className='card text-center'>
-                    <img src={avatar_url}
-                         alt="" className="round-img"
-                         style={{width: '60px'}}
-                    />
-                    <h3>{login}</h3>
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
 
-                    <div>
-                        <a href={html_url} className="btn btn-dark btn-sm my-1">
-                            More
-                        </a>
-                    </div>
+    return (
+        <div>
+            <div className='card text-center'>
+                <img src={avatar_url}
+                     alt="" className="round-img"
+                     style={{width: '60px'}}
+                />
+                <h3>{login}</h3>
+
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        More
+                    </a>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem;
